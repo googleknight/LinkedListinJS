91d740c6c7a3a5e560d7ba1a7c8f7f6475f3a99b
Shubham Mathur <shubham.mathur@engineer.com>
2018-02-08 12:04:38 +0530

    Append function added and tested

--- LinkedListByFunctions.js
+++ LinkedListByFunctions.js
@@ -2,5 +2,16 @@ function Node(data) {
   this.data = data;
   this.nextNode = null;
 }
-
-module.exports = { Node };
+Node.prototype.getLastNode = function getLastNode() {
+  let head = this;
+  while (head && head.nextNode) {
+    head = head.nextNode;
+  }
+  return head;
+};
+Node.prototype.appendInLast = function appendInLast(data) {
+  const newNode = new Node(data);
+  const lastNode = this.getLastNode();
+  lastNode.nextNode = newNode;
+};
+module.exports = Node;
--- LinkedListByFunctions.test.js
+++ LinkedListByFunctions.test.js
@@ -1,7 +1,14 @@
-const { Node } = require('./LinkedListByFunctions');
+const Node = require('./LinkedListByFunctions');
 
 describe('Testing Linked List and its functions:', () => {
   it('Node should be a method', () => {
     expect(typeof Node).toBe('function');
+  });
+  it('Method should append node in end', () => {
+    const head = new Node(0);
+    head.appendInLast(1);
+    const output = [head.data, head.nextNode.data];
+    const expected = [0, 1];
+    expect(output).toEqual(expected);
   });
 });
